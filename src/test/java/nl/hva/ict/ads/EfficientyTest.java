d4ee0829695918dba6fca1b65732422abed00cb7
KaivandenBroek <Kaivandenbroek@hotmail.com>
2021-11-07 00:19:54 +0100

    efficienty test done

--- src/test/java/nl/hva/ict/ads/EfficientyTest.java
+++ src/test/java/nl/hva/ict/ads/EfficientyTest.java
@@ -1,27 +1,21 @@
 package nl.hva.ict.ads;
 
-import org.apache.commons.lang3.time.StopWatch;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Timer;
 
 class EfficientyTest {
+    final static int MAX_ARCHERS = 5000000;
+    final static int MAX_MILLISECONDS = 20000; // 20 seconds
+
     protected Sorter<Archer> sorter = new ArcherSorter();
-    protected List<Archer> fewArchers;
-    protected List<Archer> manyArchers;
-    protected List<Archer> testArchers;
-    protected List<Archer> testArchers2;
-    protected List<Archer> testArchers3;
-    protected long duration;
-    protected long duration2;
-    Timer timer = new Timer();
-    StopWatch stopwatch = new StopWatch();
-    protected int maxArchers = 5000000;
-    protected int maxTimeInMilis = 20000;
+    protected List<Archer> archers1;
     protected Comparator<Archer> scoringScheme = Archer::compareByHighestTotalScoreWithLeastMissesAndLowestId;
 
     @BeforeEach
@@ -30,35 +24,67 @@ class EfficientyTest {
     }
 
     @Test
-    void test() {
-        // create archers
-        int amountarchers = 100;
+    void timeTest() {
+        ChampionSelector championSelector = new ChampionSelector(1L);
+        int archerAmount = 100;
+        boolean collectionsSortRunning = true;
+        boolean selSortRunning = true;
+        boolean quickSortRunning = true;
+        Stopwatch tracker1;
+        Stopwatch tracker2;
+        Stopwatch tracker3;
+        double time1 = 0;
+        double time2 = 0;
+        double time3 = 0;
 
-        while (amountarchers < maxArchers) {
+        while (archerAmount < MAX_ARCHERS) {
+            archers1 = new ArrayList<Archer>(championSelector.enrollArchers(archerAmount));
+            List<Archer> archers2 = new ArrayList<>(archers1);
+            List<Archer> archers3 = new ArrayList<>(archers1);
 
-            ChampionSelector championSelector = new ChampionSelector(1L);
-            testArchers = new ArrayList<Archer>(championSelector.enrollArchers(amountarchers));
+            System.out.println("Number of archers: " + archerAmount);
 
-            long startTime3 = System.currentTimeMillis();
-            while ((System.currentTimeMillis() - startTime3) < 100) {
-
-                long startTime = System.currentTimeMillis();
-
-                sorter.selInsSort(testArchers, scoringScheme);
-
-                long endTime = System.currentTimeMillis();
-                duration = (endTime - startTime);
-
-                System.out.println("insertion sort - amount archers: " + amountarchers
-                        + " duration test in miliseconds: " + duration);
+            if (collectionsSortRunning) {
+                tracker1 = new Stopwatch();
+                Collections.sort(archers1, scoringScheme);
+                time1 = tracker1.elapsedTime();
+                System.out.println("Collections sort: " + time1);
+                if (tracker1.elapsedTime() >= MAX_MILLISECONDS) {
+                    System.out.println("Collections sort timed out");
+                    collectionsSortRunning = false;
+                }
             }
 
-            amountarchers += amountarchers * 2;
-        }
+            if (selSortRunning) {
+                tracker2 = new Stopwatch();
+                sorter.selInsSort(archers2, scoringScheme);
+                time2 = tracker2.elapsedTime();
+                System.out.printf("Insertionsort: %.2f \n", time2);
+                if (tracker2.elapsedTime() >= MAX_MILLISECONDS) {
+                    System.out.println("Insertion sort timed out");
+                    selSortRunning = false;
+                }
+            }
 
-        System.out.println("Test done");
+            if (quickSortRunning) {
+                tracker3 = new Stopwatch();
+                sorter.quickSort(archers3, scoringScheme);
+                time3 = tracker3.elapsedTime();
+                System.out.printf("Quicksort: %.2f \n", time3);
+                if (tracker3.elapsedTime() >= MAX_MILLISECONDS) {
+                    System.out.println("Quicksort timed out");
+                    quickSortRunning = false;
+                }
+
+            }
+
+            System.out.println();
+            archerAmount *= 2;
+        }
+        // we will not be asserting Collections.sort since it could outperform our maxes
+        assertFalse(selSortRunning);
+        assertFalse(quickSortRunning);
 
         System.gc();
     }
-
 }
